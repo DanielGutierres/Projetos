a7016be42dc2c82a0357c827b2f480622a1d7e31
Daniel Gutierres <daniel113388@gmail.com>
2024-04-16 23:08:40 -0300

    chore: migrando para o angular 13

--- Angular/curso-angular/src/app/app.component.spec.ts
+++ Angular/curso-angular/src/app/app.component.spec.ts
@@ -4,7 +4,9 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      declarations: [
+        AppComponent
+      ],
     }).compileComponents();
   });
 
@@ -14,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'moments-project' title`, () => {
+  it(`should have as title 'curso-angular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('moments-project');
+    expect(app.title).toEqual('curso-angular');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, moments-project');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('curso-angular app is running!');
   });
 });
