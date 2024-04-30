ad20a0ddb6ef67feb5aa96bd3e29db5684954bd7
Daniel Gutierres <daniel113388@gmail.com>
2024-04-30 00:14:12 -0300

    diretivas angular

--- Angular/curso-angular/src/app/components/parent-data/parent-data.component.ts
+++ Angular/curso-angular/src/app/components/parent-data/parent-data.component.ts
@@ -6,8 +6,8 @@ import { Component, OnInit,Input } from '@angular/core';
   styleUrls: ['./parent-data.component.css']
 })
 export class ParentDataComponent implements OnInit {
-  @Input() name: string = '';
-  @Input() userData!: {email: string; role: string; 
+  @Input() name: string = ''; // aqui inicio o dado com o valor vazio ''
+  @Input() dataUser!: {email: string; role: string;  //já aqui com o !  para que  o Typescript entenda que ela vai ser iniciada, assim como acima
     idade: number; sexo: string; 
   }
   constructor() { }
