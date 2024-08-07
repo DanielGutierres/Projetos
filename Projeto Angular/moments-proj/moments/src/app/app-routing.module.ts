d7e054153c0a81562151f73e89e84af030884246
Daniel Gutierres <daniel113388@gmail.com>
2024-08-06 23:28:26 -0300

    ajustes de css iniciais

--- Projeto Angular/moments-proj/moments/src/app/app-routing.module.ts
+++ Projeto Angular/moments-proj/moments/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
-import { NgModule, Component } from '@angular/core';
+import { NewMomentsComponent } from './components/pages/new-moments/new-moments.component';
+import { NgModule} from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/pages/home/home.component';
 import { AboutComponent } from './components/pages/about/about.component';
 
 const routes: Routes = [
    {path: '', component: HomeComponent},  //'' significa Home 
-   {path: 'about', component: AboutComponent}
+   {path: 'about', component: AboutComponent},
+   {path: 'moments/new', component: NewMomentsComponent}
 
 ];
 
